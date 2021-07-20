2cb5e75c36fcb35138c5453e66c077febb5a6b86
matheus b silva <matheus.barretto2001@gmail.com>
2021-07-20 18:13:38 -0300

    initial commit

---
+++ src/components/style/GlobalStyle.ts
@@ -0,0 +1,13 @@
+import {createGlobalStyle} from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle `
+
+
+body {
+    color: black;
+    font-family: 'Lora', serif;
+}
+
+
+
+`;
---
+++ src/pages/app/App.tsx
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+import { GlobalStyle } from "../../components/style/GlobalStyle";
+
+
+
+function App() {
+  return (
+    <Fragment>
+      <GlobalStyle/>
+        <h1>ola mundo</h1>
+      
+    </Fragment>
+      
+  );
+}
+
+export default App;
